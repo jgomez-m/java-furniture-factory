ed305b39628e5f5a599da6b0a93945c37173a276
User <user@rba-hackerrank.com>
2019-04-11 23:59:00 +0000

    [HackerRank] Auto commit on Run Tests

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -1,22 +1,27 @@
 package hackerrank;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
+    private Map<Furniture, Integer> order;
 
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
+        order = new HashMap<Furniture, Integer>();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
-        // TODO: Complete the method
+        order.computeIfPresent(type, (k, val) -> val + furnitureCount);
+        order.putIfAbsent(type, furnitureCount);
     }
+
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
@@ -25,17 +30,23 @@ public class FurnitureOrder implements FurnitureOrderInterface {
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+        float totalCost;
+        order.forEach((k, val) -> {
+            totalCost =  k.cost() * val;
+        });
+        return totalCost;
     }
 
     public int getTypeCount(Furniture type) {
-        // TODO: Complete the method
-        return -1;
+        return order.containsKey(type) ? order.get(type) : 0;
     }
 
     public float getTypeCost(Furniture type) {
-        // TODO: Complete the method
-        return -1.0f;
+        float cost = 0.0f;
+        if(order.containsKey(type)){
+            cost = order.get(type) * type.cost();
+        }
+        return cost;
     }
 
     public int getTotalOrderQuantity() {
